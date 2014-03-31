88d72d706ec22ea431becfab35e75b8e37a55676
Filip Buszko <filipbuszko@gmail.com>
2014-04-01 01:15:31 +0200

    FutureObject class added, Refactor of AmberClient class.

---
+++ Amber_API/Amber_API/amber/FutureObject.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Amber_API.amber
+{
+    public class FutureObject
+    {
+        protected bool available = false;
+
+        protected Exception exception = null;
+        private AutoResetEvent autoEvent = new AutoResetEvent(false);
+
+        public bool isAvailable()
+        {
+            if (exception != null)
+            {
+                throw exception;
+            }
+            return available;
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public void waitAvailable()
+        {
+            try
+            {
+                while (!isAvailable())
+                {
+                    autoEvent.WaitOne();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public void setAvailable()
+        {
+            available = true;
+            autoEvent.Set();
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public void setException(Exception e)
+        {
+            available = true;
+            exception = e;
+            autoEvent.Set();
+        }
+    }
+}
