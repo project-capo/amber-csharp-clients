ca163bf31b18eb396bb033df06a90d55653d960e
Filip Buszko <filipbuszko@gmail.com>
2014-03-31 19:00:09 +0200

    Add Amber client implementation

---
+++ Amber_API/Amber_API/amber/AmberConnectionException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amber_API.amber
+{
+    public class AmberConnectionException : Exception
+    {
+        private static readonly long serialVersionUID = -7736321114486909539L;
+    }
+}
---
+++ Amber_API/Amber_API/amber/AmberProxy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using amber;
+using Google.ProtocolBuffers;
+
+namespace Amber_API.amber
+{
+    public abstract class AmberProxy
+    {
+        protected readonly AmberClient amberClient;
+        protected readonly int deviceType;
+        protected readonly int deviceID;
+
+        public AmberProxy(int deviceType, int deviceID, AmberClient amberClient)
+        {
+            this.deviceType = deviceType;
+            this.deviceID = deviceID;
+            this.amberClient = amberClient;
+
+            amberClient.RegisterClient(deviceType, deviceID, this);
+        }
+
+        public abstract void HandleDataMsg(DriverHdr header, DriverMsg message);
+
+        public void HandlePingMsg(DriverHdr header, DriverMsg message)
+        {
+        }
+
+        public void HandlePongMsg(DriverHdr header, DriverMsg message)
+        {
+        }
+
+        public void HandleDriverDiedMsg(DriverHdr header, DriverMsg message)
+        {
+        }
+
+        public abstract ExtensionRegistry GetExtensionRegistry();
+
+        protected DriverHdr BuildHeader()
+        {
+            DriverHdr.Builder driverHdrBuilder = DriverHdr.CreateBuilder();
+            driverHdrBuilder.SetDeviceType(deviceType);
+            driverHdrBuilder.SetDeviceID(deviceID);
+
+            return driverHdrBuilder.Build();
+        }
+
+    }
+}
