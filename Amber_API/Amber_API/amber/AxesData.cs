4729fb5f491a376f1c0dce3d5e6ccd940b25065d
Filip Buszko <filipbuszko@gmail.com>
2014-04-06 21:53:59 +0200

    Add Ninedof proxy with additional classes (AxesData, NinedofOutcome)

---
+++ Amber_API/Amber_API/amber/AxesData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amber_API.Amber
+{
+    public class AxesData
+    {
+        public int XAxis { get; set; }
+        public int YAxis { get; set; }
+        public int ZAxis { get; set; }
+
+        public AxesData(int xAxis, int yAxis, int zAxis)
+        {
+            XAxis = xAxis;
+            YAxis = yAxis;
+            ZAxis = zAxis;
+        }
+    }
+}
