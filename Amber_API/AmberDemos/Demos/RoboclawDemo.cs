1b9aea38411e03a5196011f9267c99b8690ec14c
Filip Buszko <filipbuszko@gmail.com>
2014-04-16 12:32:19 +0200

    demos

---
+++ Amber_API/AmberDemos/Demos/RoboclawDemo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amber.Protos;
+using Amber_API.Amber;
+using Amber_API.Drivers;
+
+namespace AmberDemos.Demos
+{
+    public class RoboclawDemo
+    {
+        public void RunDemo()
+        {
+            AmberClient client = AmberClient.Create("192.168.1.50", 26233);
+
+            var roboclawProxy = new RoboclawProxy(client, 0);
+
+            int speed = 1000;
+
+            roboclawProxy.SetSpeed(speed, speed, speed, speed);
+            var speedOutcome = roboclawProxy.GetSpeed();
+            speedOutcome.WaitAvailable();
+
+            Console.WriteLine("Motors current speed: fl: {0}, fr: {1}, rl: {2}, rr: {3}",
+                speedOutcome.FrontLeftSpeed, speedOutcome.FrontRightSpeed, speedOutcome.RearLeftSpeed, speedOutcome.RearRightSpeed);
+        }
+    }
+}
---
+++ Amber_API/Amber_API/amber/NinedofDataListener.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amber_API.Amber
+{
+    public class NinedofDataListener : IAmberListener<NinedofOutcome>
+    {
+        public void Handle(NinedofOutcome data)
+        {
+            Console.WriteLine(
+                   "Ninedof current parameters: gyro:\n\tX: {0}\n\tY: {1}\n\tZ: {2}\naccel:\n\tX: {3}\n\tY: {4}\n\tZ: {5}",
+                   data.Gyro.XAxis, data.Gyro.YAxis, data.Gyro.ZAxis,
+                   data.Accel.XAxis, data.Accel.YAxis, data.Accel.ZAxis);
+        }
+    }
+}
